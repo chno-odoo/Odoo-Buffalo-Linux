c9c0dca23aae509d37882d1dbdd253c7ee60f69a
Chris Nowak <chno@Chriss-MBP.buf.odoo.com>
2024-03-19 08:56:28 -0400

    [ADD/IMP] Added a curl of the new image as well as changed the script to use the new image as the background.

--- bash/customization.sh
+++ bash/customization.sh
@@ -5,9 +5,10 @@
 #Download the raw image files
 curl -L https://raw.githubusercontent.com/chno-odoo/Odoo-Buffalo-Linux-/5b5cb493fb4ab58e40ace10d1e88b0f57d2f27d7/images/odoo_logo.png -o $HOME/odoo_logo.png
 curl -L https://raw.githubusercontent.com/chno-odoo/Odoo-Buffalo-Linux-/5b5cb493fb4ab58e40ace10d1e88b0f57d2f27d7/images/Ologo1.png -o $HOME/Ologo1.png
+curl -L https://raw.githubusercontent.com/chno-odoo/Odoo-Buffalo-Linux-/5b5cb493fb4ab58e40ace10d1e88b0f57d2f27d7/images/odoo_logo_inverted.png -o $HOME/odoo_logo_inverted.png
 
 # Set the downloaded image as the desktop background. 
-gsettings set org.cinnamon.desktop.background picture-uri 'file://'$HOME'/odoo_logo.png'
+gsettings set org.cinnamon.desktop.background picture-uri 'file://'$HOME'/odoo_logo_inverted'
 
 # Set the background picture options.
 gsettings set org.cinnamon.desktop.background picture-options 'centered'
