e2cdc7362f3afe3e289604ba6638c28af17a7aae
Chris Nowak <chno@Chriss-MacBook-Pro.local>
2024-03-19 16:03:23 -0400

    [ADD] Added a configuration for a custom menu icon as well as the applications that are changed on the panel.

--- bash/customization.sh
+++ bash/customization.sh
@@ -16,6 +16,9 @@ gsettings set org.cinnamon.desktop.background picture-options 'centered'
 # Set the background primary color.
 gsettings set org.cinnamon.desktop.background primary-color '#FFFFFF'
 
+# Set the custom menu icon.
+gsettings set org.cinnamon.desktop.interface icon-name 'file://'$HOME'/Ologo1.png'
+
 # Set the Mouse Pointer.
 gsettings set org.cinnamon.desktop.interface cursor-theme 'DMZ-White'
 
@@ -27,3 +30,9 @@ gsettings set org.cinnamon.desktop.interface icon-theme 'Mint-L-Purple'
 
 # Set the desktop theme.
 gsettings set org.cinnamon.theme name 'Mint-L-Purple'
+
+# Pin the wanted applications to the panel.
+gsettings set org.cinnamon favorite-apps "['nemo.desktop', 'gnome.Terminal.desktop', 'google-chrome.desktop', 'disc"ord.desktop', 'code.desktop', 'libreoffice-calc.desktop', 'libreoffice-writer.desktop', 'org.flameshot.Flameshot.desktop', 'simplescreenrecorder.desktop', 'cinnamon-settings.desktop']"
+
+
+
