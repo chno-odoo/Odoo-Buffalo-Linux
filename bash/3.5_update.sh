1d75e6569c01ec727a9e6149ad7af83cd6bebcc8
Chno <chno@odoo.com>
2024-05-03 14:09:48 -0400

    [ADD] Readded Diodon GTK+ clipboard manager to the OS.

---
+++ bash/3.5_update.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# This script handles updates between Odoo Mint 2.0 and Odoo Mint 3.2.
+# It installs Thunderbird, Keepassx, Slimbook Battery, and Spotify-Client.
+
+# Ensure the script is run as root or with sudo privileges.
+if [ "$(id -u)" -ne 0 ]; then
+    echo "This script needs to be run as root or with sudo privileges."
+    exit 1
+fi
+
+# Update packages.
+apt update
+
+# Install Diodon.
+apt install diodon
