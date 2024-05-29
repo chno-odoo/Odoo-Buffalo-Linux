6614388ef4999e4aeeacc80cf0b2b5c55011cef8
Chno <chno@odoo.com>
2024-05-29 13:04:52 -0400

    [DEL] Removed Slimbook installation from backend_settings.sh. Slimbook battery was able to be installed via the ISO.

--- bash/backend_settings.sh
+++ bash/backend_settings.sh
@@ -11,15 +11,6 @@ fi
 # Add atera agent.
 (wget -O - "https://HelpdeskSupport1706554083638.servicedesk.atera.com/api/utils/AgentInstallScript/Linux/001Q3000006btKcIAI?customerId=7" | bash)
 
-echo "Adding Slimbook repository..."
-sudo add-apt-repository ppa:slimbook/slimbook -y
-
-echo "Updating packages..."
-sudo apt update
-
-echo "Installing Slimbook Battery..."
-sudo apt install slimbookbattery -y
-
 # Set the system limits and timezone.
 echo "fs.file-max = 2097152" | sudo tee -a /etc/sysctl.conf > /dev/null
 sudo timedatectl set-timezone America/New_York
